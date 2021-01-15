62b4aebfe2778ac3e5432222af6c7b4afc5af7de
madophs <jehuruv@hotmail.com>
2021-01-15 17:53:05 -0600

    Remove user configs, they will be set manually

--- scripts/plugins_enabled_user.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# All ZSH plugins enabled by User
-export ZSH_AUTOSUGGESTIONS_ENABLED="N"
-export ZSH_SYNTAX_HIGHLIGHTING_ENABLED="N"
-export ZSH_NAVIGATION_TOOLS_ENABLED="Y"
-export MDS_FANCY="NO"
-
