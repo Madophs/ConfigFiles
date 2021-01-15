cff43af4df213706ba14c083a32ee74c71254795
madophs <jehuruv@hotmail.com>
2021-01-15 17:39:44 -0600

    User configs

---
+++ scripts/plugins_enabled_user.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# All ZSH plugins enabled by User
+export ZSH_AUTOSUGGESTIONS_ENABLED="N"
+export ZSH_SYNTAX_HIGHLIGHTING_ENABLED="N"
+export ZSH_NAVIGATION_TOOLS_ENABLED="Y"
+export MDS_FANCY="NO"
+
