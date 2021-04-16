c2d60aa6a56b49371996dfd577a80cb6a9db6cfa
madophs <jehuruv@hotmail.com>
2021-04-16 00:43:46 -0500

    Improving backup steam files script

--- scripts/backup_steam_savefiles.sh
+++ scripts/backup_steam_savefiles.sh
@@ -4,16 +4,39 @@ STEAM_DIR=$HOME/.steam
 BACKUP_DIR=$HOME/MEGAsync/steam_backup
 FILE_REPORT=$BACKUP_DIR/files.txt
 
+function getCksumsFromDir() {
+    DIR=$1
+    cksum $DIR/*.sl2 | sort -k 1
+}
+
+function getCksumsFromVar() {
+    FILES=$1
+    echo $FILES | xargs cksum | sort -k 1
+}
+
 if [[ -d $STEAM_DIR && -d $BACKUP_DIR ]]; then
     FILES=$(find $STEAM_DIR -name "*.sl2" -printf '"%p"\n' | tee $FILE_REPORT)
     echo $FILES | xargs cp -t $BACKUP_DIR
-    echo "Files have been successfully backed up"
-    echo "Original cksums"
-    echo $FILES | xargs cksum | sort -k 1 | awk -F '[/ ]' '{print $1" "$2" "$(NF)}'
-    echo ""
-    echo "Destination cksums"
-    cksum $BACKUP_DIR/*.sl2 | sort -k 1 | awk -F '[/ ]' '{print $1" "$2" "$(NF)}'
+
+    if [[ $? == 0 ]]; then
+        echo "Files have been successfully backed up"
+    else
+        echo "[ERROR] Something went wrong."
+        exit 1
+    fi
+
+    printf "\nOriginal cksums\n"
+    column -c 4 \
+        <( paste  <( column -enxt <(getCksumsFromVar "${FILES}" | awk -F '[/ ]' '{print $1" "$2" "$(NF)" "}' ) )\
+        <( getCksumsFromVar "${FILES}" | awk -F '[/]' '{print $(NF-2)}') )
+
+    printf "\nBackup cksums\n"
+
+    column -c 4 \
+        <( paste  <( column -enxt <(getCksumsFromDir $BACKUP_DIR | awk -F '[/ ]' '{print $1" "$2" "$(NF)" "}' ) )\
+        <( getCksumsFromVar "${FILES}" | awk -F '[/]' '{print $(NF-2)}') )
 else
     echo "[ERROR] Steam or Backup directory not found..."
     exit 1
 fi
+
