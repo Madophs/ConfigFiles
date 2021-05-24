f7857310a4bd064198008b453f171f56a81067fd
Jehú jair <tleveredict@gmail.com>
2021-05-23 23:52:59 -0500

    Git push expect
    An interactive Git Push scripts to automatically populate user and
    password.

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ $# > 1 ]]
+then
+    echo "Too many arguments..."
+    exit 1
+fi
+
+# Good for public repos
 git_push() {
     git rev-parse --show-toplevel &> /dev/null
     if [[ $? == 0 ]]
@@ -13,4 +20,30 @@ git_push() {
     fi
 }
 
-git_push
+# Good for push to remote private repos
+git_push_force() {
+    git status &> /dev/null
+    if [[ $? == 0 ]]
+    then
+        TOKEN=$($MDS_CONFIG/scripts/handle_git_token.sh show)
+        $MDS_CONFIG/scripts/git_push_expect.exp ${TOKEN}
+    else
+        echo "[ERROR] Not a git repo."
+        exit 1
+    fi
+}
+
+if [[ $# == 1 ]]
+then
+    case $1 in
+        force)
+            git_push_force
+        ;;
+        *)
+            echo -n "[ERROR] Unknown option."
+        ;;
+    esac
+else
+    git_push
+fi
+
