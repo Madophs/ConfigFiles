b8e40de37c3b5a20886dd0707719d7ab598c69ec
madophs <jehuruvj@gmail.com>
2026-03-24 02:08:08 -0600

    Add bashrc specific for bash configs

---
+++ configs/bash/bashrc.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+trap 'eval "$(< "${MDS_TRAP_CMD}")"; truncate -s 0 "${MDS_TRAP_CMD}"' SIGRTMIN+1
+
+HISTSIZE=7000
+HISTFILESIZE=7000
+export HISTCONTROL='erasedups:ignoreboth'
+unset -v HISTTIMEFORMAT  # %F %T
+
+shopt -s autocd
+shopt -s cdable_vars
+shopt -s extglob
+shopt -s histappend
+shopt -s nocaseglob
+shopt -s nocasematch
+shopt -s nullglob
+bind 'set show-all-if-ambiguous on'
+bind -s 'set completion-ignore-case on'
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+
+if [[ -f "${GIT_REPOS}/Bash-Complete-Menu/bash_complete_menu.sh" ]]
+then
+    source "${GIT_REPOS}/Bash-Complete-Menu/bash_complete_menu.sh"
+    bind -x '"\C-i":bash_complete_menu'
+else
+    bind 'TAB:complete'
+fi
+
+if [[ ! -h "${HOME}/.inputrc" ]]
+then
+    rm -f "${HOME}/.inputrc"
+    ln -s "${MDS_CONFIG}/configs/bash/inputrc" "${HOME}/.inputrc"
+fi
