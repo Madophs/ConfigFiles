0700172ab70a3886d68466a972b9735f5ace2c47
madophs <jehuruvj@gmail.com>
2026-03-24 01:53:01 -0600

    single quote most aliases

--- configs/aliases.sh
+++ configs/aliases.sh
@@ -1,24 +1,24 @@
 # Directories aliases
-alias cdgit="cd ${GIT_REPOS}"
-alias lcpp="cd ${GIT_REPOS}/C-CPP-Linux-Programming"
-alias cpp="cd ${CTEST}"
-alias cdtest="cd ${TEST}"
-alias artisan="php ${MDS_ROOT}/artisan"
+alias cdgit='cd ${GIT_REPOS}'
+alias lcpp='cd ${GIT_REPOS}/C-CPP-Linux-Programming'
+alias cpp='cd ${CTEST}'
+alias cdtest='cd ${TEST}'
+alias artisan='php ${MDS_ROOT}/artisan'
 alias cdhtml="cd /var/www/html/"
-alias cddir="cd ${HOME}/Documents/"
-alias cdconfig="cd ${MDS_CONFIG}"
-alias cdscripts="cd ${MDS_SCRIPTS}"
-alias cdw="cd ${HOME}/Downloads"
-alias cdd="cd ${HOME}/Documents"
+alias cddir='cd ${HOME}/Documents/'
+alias cdconfig='cd ${MDS_CONFIG}'
+alias cdscripts='cd ${MDS_SCRIPTS}'
+alias cdw='cd ${HOME}/Downloads'
+alias cdd='cd ${HOME}/Documents'
 alias cdg='TMPGITDIR=$(git rev-parse --show-toplevel 2> /dev/null) && cd ${TMPGITDIR}; unset TMPGITDIR'
-alias cdapps="cd ${MDS_APPS}"
+alias cdapps='cd ${MDS_APPS}'
 
 #command aliases
-alias cpptags="cd ${MDS_ROOT}; rm -f tags; ctags -R --c++-kinds=+p; export TAGS=${MDS_ROOT}/tags"
-alias phptags="cd ${MDS_ROOT}; rm -f tags; ctags -R --languages=php --exclude=storage; export TAGS=\${MDS_ROOT}/tags"
-alias setroot="echo \$(pwd) > ${MDS_ROOT_FILE}; export MDS_ROOT=\$(cat \${MDS_ROOT_FILE}); export TAGS=\${MDS_ROOT}/tags"
+alias cpptags='cd "${MDS_ROOT}"; rm -f tags; ctags -R --c++-kinds=+p; export TAGS="${MDS_ROOT}/tags"'
+alias phptags='cd "${MDS_ROOT}"; rm -f tags; ctags -R --languages=php --exclude=storage; export TAGS="${MDS_ROOT}/tags"'
+alias setroot='echo $(pwd) > "${MDS_ROOT_FILE}"; export MDS_ROOT=$(cat "${MDS_ROOT_FILE}"); export TAGS="${MDS_ROOT}/tags"'
 alias cdr='MDS_ROOT="$(cat "${MDS_ROOT_FILE}")"; cd "${MDS_ROOT}" 2> /dev/null'
-alias getroot="export MDS_ROOT=\$(cat ${MDS_ROOT_FILE}); echo \${MDS_ROOT}"
+alias getroot='export MDS_ROOT=$(cat ${MDS_ROOT_FILE}); echo ${MDS_ROOT}'
 alias upgrade="export IS_APT_UPDATE_PERFORMED=YES && sudo apt update && sudo apt upgrade -y"
 alias update="export IS_APT_UPDATE_PERFORMED=YES && sudo apt update"
 alias install="sudo apt install"
@@ -28,14 +28,14 @@ alias autoremove="sudo apt autoremove"
 alias remove="sudo apt remove"
 alias show="apt show"
 alias search="sudo apt search"
-alias zshrc="${EDITOR} ~/.zshrc"
+alias zshrc='${EDITOR} ~/.zshrc'
 alias zshload="source ~/.zshrc"
 alias zshlogin="exec zsh --login"
 alias off="qdbus org.kde.Shutdown /Shutdown org.kde.Shutdown.logoutAndShutdown"
 alias logout="qdbus org.kde.Shutdown /Shutdown logout"
 alias nap="qdbus org.kde.Solid.PowerManagement /org/kde/Solid/PowerManagement/Actions/SuspendSession org.kde.Solid.PowerManagement.Actions.SuspendSession.suspendToRam"
 alias myip="host myip.opendns.com resolver1.opendns.com"
-alias vimrc="${EDITOR} ${MDS_CONFIG}/vimrc"
+alias vimrc='${EDITOR} ${MDS_CONFIG}/vimrc'
 alias config='${EDITOR} $(find ${MDS_CONFIG} -type f -not -path "*/[@.]*" | fzf)'
 alias mm="mdscode -f cpp -t -n"
 alias mb="mdscode -b"
@@ -43,28 +43,31 @@ alias me="mdscode -e"
 alias mo="mdscode -o -n"
 alias vimc="vim --servername Competitive --remote-silent "
 alias nvimf='nvim "$(fzf)"'
-alias nviml="nvim \$(ls -A | fzf)"
+alias nviml='nvim $(ls -A | fzf)'
 alias cdl='cd "$(fzf | grep -o -e ".\+\/")"'
-alias syscalls="batcat \$(find /usr/include -name 'unistd*.h' | xargs grep 'write 1' | head -n 1 | awk -F ':' '{print \$1}')"
 alias bashconfig="source ~/.bashrc"
 alias sane="stty sane"
 alias mdsdebugx='touch /tmp/mds_output; export MDS_DEBUG=/tmp/mds_output'
 alias mdsdebugc='truncate -s 0 /tmp/mds_output'
 alias mdsdebugw='watch -n 1 "tac /tmp/mds_output | head -n 50"'
 
+# positional arguments are passed by awk command
+# shellcheck disable=SC2142
+alias syscalls="batcat \$(find /usr/include -name 'unistd*.h' | xargs grep 'write 1' | head -n 1 | awk -F ':' '{print \$1}')"
+
 # Scripts aliases
-alias operaffmpeg="${MDS_SCRIPTS}/operaffmpeg.sh"
-alias zshplugins="${MDS_SCRIPTS}/zsh/zsh_plugins_setup.sh"
-alias lfunc="source ${MDS_SCRIPTS}/custom_cmds.sh"
-alias sl2="${MDS_SCRIPTS}/backup_steam_savefiles.sh"
-alias htoken="${MDS_SCRIPTS}/handle_git_token.sh"
-alias gclone="${MDS_SCRIPTS}/git.sh clone"
-alias gpush="${MDS_SCRIPTS}/git.sh push"
-alias gpull="${MDS_SCRIPTS}/git.sh pull"
-alias gfetch="${MDS_SCRIPTS}/git.sh fetch"
+alias operaffmpeg='${MDS_SCRIPTS}/operaffmpeg.sh'
+alias zshplugins='${MDS_SCRIPTS}/zsh/zsh_plugins_setup.sh'
+alias lfunc='source ${MDS_SCRIPTS}/custom_cmds.sh'
+alias sl2='${MDS_SCRIPTS}/backup_steam_savefiles.sh'
+alias htoken='${MDS_SCRIPTS}/handle_git_token.sh'
+alias gclone='${MDS_SCRIPTS}/git.sh clone'
+alias gpush='${MDS_SCRIPTS}/git.sh push'
+alias gpull='${MDS_SCRIPTS}/git.sh pull'
+alias gfetch='${MDS_SCRIPTS}/git.sh fetch'
 alias gvdiff="git difftool --tool=nvimdiff"
-alias pdir="source ${MDS_SCRIPTS}/zsh/hotlist.sh; push_directory_to_hotlist \$(pwd)"
-alias rdir="source ${MDS_SCRIPTS}/zsh/hotlist.sh; remove_directory_from_hotlist \$(pwd)"
+alias pdir='source ${MDS_SCRIPTS}/zsh/hotlist.sh; push_directory_to_hotlist $(pwd)'
+alias rdir='source ${MDS_SCRIPTS}/zsh/hotlist.sh; remove_directory_from_hotlist $(pwd)'
 alias ncd="n-cd"
 
 if [[ -f "${GIT_REPOS}/alias_completion/complete_alias" ]]
