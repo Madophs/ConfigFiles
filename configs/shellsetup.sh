adc84331cb327c7fb08112ad5d4b1a0114145a0a
madophs <jehuruvj@gmail.com>
2026-03-24 02:01:42 -0600

    shellsetup: re-structuring

--- configs/shellsetup.sh
+++ configs/shellsetup.sh
@@ -1,61 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
-# Shell configurations
-HISTSIZE=3000
-HISTFILESIZE=3000
+# necessary files and directories
+mkdir -p "${MDS_HIDDEN_CONFIGS}"
+mkdir -p "${MDS_SESSIONS_DIR}"
+touch "${MDS_TRAP_CMD}"
+touch "${MDS_ROOT_FILE}"
 
-# User VI like map keys
-set -o vi
-
-# Disable error when overriding a file
-set +o noclobber
-
-# trap to execute any command
-trap "eval \"\$(< ${MDS_TRAP_CMD})\"; truncate -s 0 ${MDS_TRAP_CMD}" 35
-
-if [[ ${REAL_SHELL} == 'bash' ]]
-then
-    source "${GIT_REPOS}/Bash-Complete-Menu/bash_complete_menu.sh"
-    shopt -s autocd
-    shopt -s cdable_vars
-    shopt -s extglob
-    shopt -s histappend
-    shopt -s nocaseglob
-    shopt -s nocasematch
-    shopt -s nullglob
-    bind 'set show-all-if-ambiguous on'
-    bind -s 'set completion-ignore-case on'
-    bind -x '"\C-i":bash_complete_menu'
-    #bind 'TAB:complete'
-    bind '"\e[A": history-search-backward'
-    bind '"\e[B": history-search-forward'
-    export HISTCONTROL='erasedups:ignoreboth'
-    unset -v HISTTIMEFORMAT  # %F %T
-fi
-
-source ${MDS_SCRIPTS}/autocomplete.sh
-
-mkdir -p ${MDS_HIDDEN_CONFIGS}
-mkdir -p ${MDS_SESSIONS_DIR}
-
-touch ${MDS_TRAP_CMD}
-
-if [[ ! -f  ${MDS_ROOT_FILE} ]]
-then
-    touch ${MDS_ROOT_FILE}
-fi
-
-# Symbolic links
-if [[ ! -h ~/.local/bin/mdssetup ]]
+if [[ ! -h "${HOME}/.local/bin/mdssetup" ]]
 then
     mkdir -p ~/.local/bin
-    ln -s -T ${MDS_SCRIPTS}/mdssetup.sh ~/.local/bin/mdssetup
+    ln -s -T "${MDS_SCRIPTS}/mdssetup.sh" ~/.local/bin/mdssetup
 fi
 
-if [[ ! -h /etc/bash_completion.d/mdscode-prompt && -f ${GIT_REPOS}/MdsCode_Bash/src/mds-prompt ]]
+if [[ ! -h /etc/bash_completion.d/mdscode-prompt && -f "${GIT_REPOS}/MdsCode_Bash/src/mds-prompt" ]]
 then
-    cout info "Setting up mdscode prompt"
-    sudo ln -s -T ${GIT_REPOS}/MdsCode_Bash/src/mds-prompt /etc/bash_completion.d/mdscode-prompt
+    sudo ln -s -T "${GIT_REPOS}/MdsCode_Bash/src/mds-prompt" /etc/bash_completion.d/mdscode-prompt
 fi
 
 if [[ -f /usr/share/autojump/autojump.bash ]]
@@ -63,22 +22,19 @@ then
     source /usr/share/autojump/autojump.bash
 fi
 
-# Let's ignore some common commands of being registered from history
-zshaddhistory() {
-    # Get the command the remove the carriage return
-    INPUT_COMMAND=${1%$'\n'}
-    case ${INPUT_COMMAND} in
-        ls|ls\ *|ll)
-            return 1
-            ;;
-        clear)
-            return 1
-            ;;
-        cd)
-            return 1
-            ;;
-        loadsh|setroot)
-            return 1;
-    esac
-    return 0;
-}
+# User VI like map keys
+set -o vi
+
+# Disable error when overriding a file
+set +o noclobber
+
+if [[ "${REAL_SHELL}" == "zsh" ]]
+then
+    source "${MDS_CONFIG}/configs/zsh/zshrc.sh"
+else
+    source "${MDS_CONFIG}/configs/bash/bashrc.sh"
+fi
+
+source "${MDS_CONFIG}/configs/aliases.sh"
+source "${MDS_CONFIG}/configs/autocomplete.sh"
+
