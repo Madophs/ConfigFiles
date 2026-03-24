527747b9ae07cdb06d49e228e44db0d1c5b9f2e1
madophs <jehuruvj@gmail.com>
2026-03-24 02:07:36 -0600

    zsh specific configs

--- configs/zsh/zshrc.sh
+++ configs/zsh/zshrc.sh
@@ -1,7 +1,6 @@
-if [[ ${REAL_SHELL} != zsh ]]
-then
-    return 0
-fi
+#!/bin/env zsh
+
+trap 'eval "$(< "${MDS_TRAP_CMD}")"; truncate -s 0 "${MDS_TRAP_CMD}"' RTMIN+1
 
 setopt HIST_IGNORE_DUPS
 setopt HIST_IGNORE_ALL_DUPS
@@ -18,5 +17,26 @@ function print-exit-code() {
     print -r -- '' ${(%):-"❌ %F{red}exit code $code%f"} ''
   fi
 }
+
+# Let's ignore some common commands of being registered from history
+zshaddhistory() {
+    # Get the command the remove the carriage return
+    INPUT_COMMAND=${1%$'\n'}
+    case ${INPUT_COMMAND} in
+        ls|ls\ *|ll)
+            return 1
+            ;;
+        clear)
+            return 1
+            ;;
+        cd)
+            return 1
+            ;;
+        loadsh|setroot)
+            return 1;
+    esac
+    return 0;
+}
+
 autoload -Uz add-zsh-hook
 add-zsh-hook precmd print-exit-code
