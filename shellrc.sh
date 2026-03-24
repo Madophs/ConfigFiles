339397dc8b9a07c3e3ce0519df124b8dd8fdbb72
madophs <jehuruvj@gmail.com>
2026-03-24 02:32:04 -0600

    shellrc: fix shellcheck warnings

--- shellrc.sh
+++ shellrc.sh
@@ -1,48 +1,54 @@
 # We can only return from a function or sourced script
-(return 0 2>/dev/null) && declare -g IS_SOURCED=YES || declare -g IS_SOURCED=NO
+if (return 0 2>/dev/null);
+then
+    declare -g IS_SOURCED=YES
+else
+    declare -g IS_SOURCED=NO
+fi
 
 # Current shell PID
-declare -g -i SHELL_PID=$(ps -opid $$ | tail -n 1)
+SHELL_PID="$(ps -opid $$ | tail -n 1)"
 
 # compute base directory
-declare -g REAL_SHELL=$(ps -o command $$ | tail -n 1 | awk '{print $0}' | grep -o -e '^[\/a-z-][/a-z]\+' | awk -F '[/-]' '{print $NF}')
+REAL_SHELL="$(ps -o command $$ | tail -n 1 | awk '{print $0}' | grep -o -e '^[\/a-z-][/a-z]\+' | awk -F '[/-]' '{print $NF}')"
 if [[ ${REAL_SHELL} == zsh ]]
 then
-    export MDS_CONFIG=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)/$(basename -- "$0")" | sed 's/\/mds_config\.sh//g')
+    MDS_CONFIG=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)/$(basename -- "$0")" | sed 's/\/shellrc\.sh//g')
 else
-    export MDS_CONFIG="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+    MDS_CONFIG="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 fi
 
 # env variables
 export SHELL_PID
-export MDS_SCRIPTS=${MDS_CONFIG}/scripts
-export MDS_SCRIPTS_BIN=${MDS_CONFIG}/scripts/bin
-export MDS_APPS=${HOME}/Documents/apps
-export MDS_HOME=${HOME}
-export MDS_ASSETS=${GIT_REPOS}/assets
-export MDS_HIDDEN_CONFIGS=${HOME}/.config/mdsconfig
-export MDS_SESSIONS_DIR=${HOME}/.config/mdsconfig/sessions
-export MDS_ROOT_FILE=${MDS_HIDDEN_CONFIGS}/.path.txt
-export MDS_ROOT=$(cat ${MDS_ROOT_FILE} 2> /dev/null)
-export MDS_TRAP_CMD=${MDS_HIDDEN_CONFIGS}/mds_trap_cmd
-export MDS_INPUT=${HOME}/.local/share/mdscode/io/input
-export MDS_OUTPUT=${HOME}/.local/share/mdscode/io/output
+export MDS_CONFIG
+export MDS_SCRIPTS="${MDS_CONFIG}/scripts"
+export MDS_SCRIPTS_BIN="${MDS_CONFIG}/scripts/bin"
+export MDS_APPS="${HOME}/Documents/apps"
+export MDS_HOME="${HOME}"
+export MDS_ASSETS="${GIT_REPOS}/assets"
+export MDS_HIDDEN_CONFIGS="${HOME}/.config/mdsconfig"
+export MDS_SESSIONS_DIR="${HOME}/.config/mdsconfig/sessions"
+export MDS_ROOT_FILE="${MDS_HIDDEN_CONFIGS}/.path.txt"
+export MDS_ROOT ; MDS_ROOT="$(cat "${MDS_ROOT_FILE}" 2> /dev/null)"
+export MDS_TRAP_CMD="${MDS_HIDDEN_CONFIGS}/mds_trap_cmd"
+export MDS_INPUT="${HOME}/.local/share/mdscode/io/input"
+export MDS_OUTPUT="${HOME}/.local/share/mdscode/io/output"
 export MDS_FANCY=YES
-export GIT_REPOS=/home/${USER}/Documents/git
-export TEST=${HOME}/Documents/test
-export CTEST=${TEST}/cpp
-export PY_IMG=${GIT_REPOS}/Image-Processsing/resources
+export GIT_REPOS="/home/${USER}/Documents/git"
+export TEST="${HOME}/Documents/test"
+export CTEST="${TEST}/cpp"
+export PY_IMG="${GIT_REPOS}/Image-Processsing/resources"
 export EDITOR=nvim
 export EDITOR_COMMAND="nvr -s {{FILE}}"
 export PAGER=less
 export LD_LIBRARY_PATH=/usr/local/cuda-11.7/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export GREP_COLORS='ms=01;32'
-export TAGS=${MDS_ROOT}/tags
+export TAGS="${MDS_ROOT}/tags"
 export PATH=/usr/local/cuda-11.7/bin${PATH:+:${PATH}}
-export PATH=${HOME}/.local/bin:${PATH}
-export PATH=${MDS_APPS}/lua-language-server/bin:${PATH}
-export PATH=${GIT_REPOS}/MdsCode_Bash:${PATH}
-export PATH=${MDS_SCRIPTS_BIN}:${PATH}
+export PATH="${HOME}/.local/bin:${PATH}"
+export PATH="${MDS_APPS}/lua-language-server/bin:${PATH}"
+export PATH="${GIT_REPOS}/MdsCode_Bash:${PATH}"
+export PATH="${MDS_SCRIPTS_BIN}:${PATH}"
 export APPCWD=/tmp/appcwd
 unset SSH_ASKPASS
 
@@ -53,12 +59,10 @@ case $- in
 esac
 
 # Small setup
-source "${MDS_SCRIPTS}/utils/cout.sh"
-source "${MDS_SCRIPTS}/utils/clock_timer.sh"
-source "${MDS_SCRIPTS}/zsh/configs.sh"
-source "${MDS_SCRIPTS}/shellsetup.sh"
-source "${MDS_SCRIPTS}/aliases.sh"
-source "${MDS_SCRIPTS}/zsh/zsh_plugins.sh"
+source "${MDS_CONFIG}/scripts/utils/cout.sh"
+source "${MDS_CONFIG}/scripts/utils/clock_timer.sh"
+source "${MDS_CONFIG}/configs/shellsetup.sh"
+source "${MDS_CONFIG}/scripts/zsh/zsh_plugins.sh"
 
 # function commands
-source ${MDS_SCRIPTS}/custom_cmds.sh
+source "${MDS_CONFIG}/scripts/custom_cmds.sh"
